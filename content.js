3475a6957d7606e7017aca38beec1b50ee66089b
Ali Wangara <Aliwangara63@gmail.com>
2025-07-22 15:00:10 +0300

    created content js to handle input logic

---
+++ content.js
@@ -0,0 +1,31 @@
+const noteEl = document.getElementById('note-el');
+const headingElement = document.querySelector('.heading-element');
+const saveNoteBtn = document.getElementById('save-note-btn');
+
+
+
+
+saveNoteBtn.addEventListener('click', () => {
+    const heading = headingElement.value.trim();
+    const note = noteEl.value.trim();
+
+    if (!heading && !note) return;
+    const noteStorage = JSON.parse(localStorage.getItem('notesEntities')) || [];
+    localStorage.clear(noteStorage)
+
+    noteStorage.push({ heading, note });
+
+   console.log ("save Btn:", localStorage.setItem('notesEntities', JSON.stringify(noteStorage)));
+
+    window.dispatchEvent(new Event('storage'))
+
+    headingElement.value = '';
+    noteEl.value = '';
+
+    window.location.href= "index.html"
+
+    console.log({ heading, note });
+});
+
+
+
